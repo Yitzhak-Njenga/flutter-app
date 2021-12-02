6a22fda4286206a8e1f990c7e8f206b08d2dd06d
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2021-12-02 19:46:56 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp( MaterialApp(
+
+  debugShowCheckedModeBanner: false,
+
+  home: Home(),
+
+));
+
+class Home extends StatefulWidget {
+
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  int myLevel = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[700],
+      appBar: AppBar(
+        title: Text('Id Project'),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.grey[600],
+      ),
+
+
+        floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          setState(() {
+            myLevel += 1;
+          });
+        },
+
+        child: Icon(Icons.add),
+          backgroundColor: Colors.white10,
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('assets/img.png'),
+                radius: 50.0,
+
+              ),
+            ),
+
+            Divider(
+              height: 60.0,
+              color: Colors.white54,
+            ),
+            Text(
+              'NAME',
+            style: TextStyle(
+              color: Colors.grey,
+              letterSpacing: 3.0,
+
+            )
+              ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+                'Yitzhak Njenga',
+                style: TextStyle(
+                  color: Colors.amber,
+                  letterSpacing: 3.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold
+
+                )
+            ),
+            SizedBox(
+              height: 30.0),
+
+              Text(
+                  'Current Level',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    letterSpacing: 2.0,
+
+                  )
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                  '$myLevel',
+                  style: TextStyle(
+                      color: Colors.amber,
+                      letterSpacing: 3.0,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+
+                  )
+              ),
+            SizedBox(
+              height: 30.0,
+            ),
+
+            Row(
+              children:const <Widget> [
+                Icon(CupertinoIcons.mail,
+                color: Colors.grey,
+                ),
+                SizedBox(width: 15.0),
+                Text('yitzhaknjenga@gmail.com',
+                style: TextStyle(
+                  color: Colors.white12,
+                  fontSize: 18.0,
+                  letterSpacing: 4
+                ),
+                )
+
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
